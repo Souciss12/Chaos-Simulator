82bcee034d42ea16634a99c9533d825e9e5feccf
Souciss12 <a12c4g@gmail.com>
2025-12-02 15:39:38 +0100

    ➕Added a basic HUD and added a store for Chaos with Pinia

--- resources/js/pages/game.vue
+++ resources/js/pages/game.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="game">
+    <div v-if="!chaosStore.isGameOver" class="game">
+        <HUD />
         <Engine />
+    </div>
+    <div v-else class="text-center">
+        <h1>Game Over !</h1>
+        <p>Your chaos level reached {{ chaosStore.chaosLevel }}%.</p>
+        <button @click="chaosStore.restart" class="btn btn-primary">Restart</button>
     </div>
 </template>
 
 <script setup>
-import Engine from '../components/engine.vue'
+import Engine from "../components/engine.vue";
+import HUD from "../components/hud.vue";
+import { useChaosStore } from "../stores/chaosStore";
+
+const chaosStore = useChaosStore();
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
