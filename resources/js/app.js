2ebab1620315b4ecb1ace6724a48cc69a18cab23
Golay Simon <a12c4g@gmail.com>
2025-12-02 13:31:26 +0100

    Initial project

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import Game from './pages/game.vue';
+import './bootstrap';
+
+createApp(Game).mount("#app");
---
+++ resources/js/pages/game.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="game">
+        <Engine />
+    </div>
+</template>
+
+<script setup>
+import Engine from '../components/engine.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
